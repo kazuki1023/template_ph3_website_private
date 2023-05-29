b97b9283900b55d5be9c2b61b7bdf56361169678
kazuki1023 <k.keio12569@gmail.com>
2023-05-29 23:16:43 +0900

    topページを持ってきた

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+
+// topページのディレクトリ
+Route::get('/', function () {
+    return view('index');
+});
 
