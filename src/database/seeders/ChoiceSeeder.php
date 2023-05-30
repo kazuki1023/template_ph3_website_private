5fe39588c84687d8eeae18f75fb3938f80f56df4
kazuki1023 <k.keio12569@gmail.com>
2023-05-30 10:25:08 +0900

    add: choiceのseeder

--- src/database/seeders/ChoiceSeeder.php
+++ src/database/seeders/ChoiceSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChoiceSeeder extends Seeder
 {
@@ -13,5 +14,43 @@ class ChoiceSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table("choices")->insert([
+            'question_id' => 1,
+            'name' => 'Manila',
+            'valid' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ], [
+            'question_id' => 1,
+            'name' => 'Tokyo',
+            'valid' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ], [
+            'question_id' => 1,
+            'name' => 'Osaka',
+            'valid' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ], [
+            'question_id' => 2,
+            'name' => 'London',
+            'valid' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ], [
+            'question_id' => 2,
+            'name' => 'Tokyo',
+            'valid' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ], [
+            'question_id' => 2,
+            'name' => 'New York',
+            'valid' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
+
 }
