ad85cab1a68f94bbb11c0cea503dbfa1b4c79707
kazuki1023 <k.keio12569@gmail.com>
2023-05-30 09:33:38 +0900

    add questoinsテーブル作成

--- src/database/migrations/2023_05_30_001655_create_questions_table.php
+++ src/database/migrations/2023_05_30_001655_create_questions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+// 問題テーブル
 return new class extends Migration
 {
     /**
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string("content", 255)->nullable()->comment("問題文");
+            $table->string("image", 255)->nullable()->comment("問題画像");
+            $table->string("supplement", 255)->nullable()->comment("補足");
             $table->timestamps();
         });
     }
