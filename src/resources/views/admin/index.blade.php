e610878a086a29d3962281ffd17cf240eaf19bc9
kazuki1023 <k.keio12569@gmail.com>
2023-06-01 12:00:35 +0900

    テーブルの見た目完成

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,12 +1,63 @@
 @extends('layouts.admin')
 
 @section('content')
-    @foreach ($questions as $question)
+    {{-- @foreach ($questions as $question)
         <div class="card h-full">
             <div class="card-body">
                 <h5 class="card-title">{{ $question->id }}</h5>
                 <p class="card-text">{{ $question->content }}</p>
             </div>
         </div>
-    @endforeach
+    @endforeach --}}
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        問題番号
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        問題文
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        選択肢１
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        選択肢２
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        選択肢３
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        詳細
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($questions as $question)
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $question->id }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $question->content }}
+                    </td>
+                    <td class="px-6 py-4">
+                        選択肢１
+                    </td>
+                    <td class="px-6 py-4">
+                        選択肢2
+                    </td>
+                    <td class="px-6 py-4">
+                        選択肢3
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">詳細</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
