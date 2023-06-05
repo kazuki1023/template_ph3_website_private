eb0b85440469d8ac5925a60a37b358e5d3fd9f71
kazuki1023 <k.keio12569@gmail.com>
2023-06-06 03:54:04 +0900

    add 編集画面にボタン設置、削除可能

--- src/resources/views/admin/edit.blade.php
+++ src/resources/views/admin/edit.blade.php
@@ -26,11 +26,11 @@
                         問題文
                     </th>
                     <td class="px-6 py-4">
-                      <div class="">
-                        <input type="text" id="default-input"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            value="{{ $question->content }}">
-                    </div>
+                        <div class="">
+                            <input type="text" id="default-input"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value="{{ $question->content }}">
+                        </div>
                     </td>
                 </tr>
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
@@ -62,11 +62,11 @@
                         画像
                     </th>
                     <td class="px-6 py-4">
-                      <div class="">
-                        <input type="text" id="default-input"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            value="{{ $question->id }}">
-                    </div>
+                        <div class="">
+                            <input type="text" id="default-input"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value="{{ $question->id }}">
+                        </div>
                     </td>
                 </tr>
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
@@ -74,14 +74,22 @@
                         補足
                     </th>
                     <td class="px-6 py-4">
-                      <div class="">
-                        <input type="text" id="default-input"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            value="{{ $question->supplement }}">
-                    </div>
+                        <div class="">
+                            <input type="text" id="default-input"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value="{{ $question->supplement }}">
+                        </div>
                     </td>
                 </tr>
             </tbody>
         </table>
+    </div>
+    <div class="content-center mt-2 flex justify-center">
+        <button type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onclick="location.href='http://localhost/admin/edit/{{ $question->id }}'">編集する</button>
+        <button type="button"
+            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            onclick="location.href='http://localhost/admin/delete/{{ $question->id }}'">削除する</button>
     </div>
 @endsection
