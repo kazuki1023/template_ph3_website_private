199152f827f0582f07a7c91d8ef2c5ab5bb32541
kazuki1023 <k.keio12569@gmail.com>
2023-06-06 01:29:48 +0900

    add 問題がない場合にalertでお知らせ

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -11,149 +11,171 @@
     @endforeach --}}
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-4 mt-3">
-        <table class=" text-sm text-left text-gray-500 w-full ">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 ">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        問題番号
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        問題文
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        詳細
-                    </th>
-                    <th scope="col" class="px-6 py-3" data-modal-target="defaultModal" data-modal-toggle="defaultModal">
-                        削除
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($questions as $question)
-                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ $question->id }}
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ $question->content }}
-                    </td>
-                    <td class="px-6 py-4">
-                        <a href="http://localhost/admin/detail/{{ $question->id }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">詳細</a>
-                    </td>
-                    <td class="px-6 py-4">
-                        <button type="button" class="font-medium text-red-600 dark:text-red-500 hover:underline" data-modal-target="defaultModal" data-modal-toggle="defaultModal" data-problem-id="{{ $question->id }}">削除</button>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-        <!-- Main modal -->
-    <div id="defaultModal" tabindex="-1" aria-hidden="true"
-        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-2xl max-h-full">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <!-- Modal header -->
-                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        以下の問題を削除しますか？
-                    </h3>
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="defaultModal">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    項目名
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    内容
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    問題名
-                                </th>
-                                <td class="px-6 py-4">
-                                    Silver
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    問題文
-                                </th>
-                                <td class="px-6 py-4">
-                                    {{ $question->content }}
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    選択肢１
-                                </th>
-                                <td class="px-6 py-4">
-                                    選択肢１
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    選択肢２
-                                </th>
-                                <td class="px-6 py-4">
-                                    選択肢２
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    選択肢３
-                                </th>
-                                <td class="px-6 py-4">
-                                    選択肢３
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    画像
-                                </th>
-                                <td class="px-6 py-4">
-                                    {{ $question->image }}
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    補足
-                                </th>
-                                <td class="px-6 py-4">
-                                    {{ $question->supplement }}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- Modal footer -->
-                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <form action="http://localhost/admin/delete/{{ $question->id }}">
-                    <button data-modal-hide="defaultModal" type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">削除する</button>
-                    </form>
-                    <button data-modal-hide="defaultModal" type="button"
-                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">削除しない</button>
+        @if ($questions->count() > 0)
+            <table class=" text-sm text-left text-gray-500 w-full ">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 ">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            問題番号
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            問題文
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            詳細
+                        </th>
+                        <th scope="col" class="px-6 py-3" data-modal-target="defaultModal"
+                            data-modal-toggle="defaultModal">
+                            削除
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($questions as $question)
+                        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $question->id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $question->content }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="http://localhost/admin/detail/{{ $question->id }}"
+                                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">詳細</a>
+                            </td>
+                            <td class="px-6 py-4">
+                                <button type="button" class="font-medium text-red-600 dark:text-red-500 hover:underline"
+                                    data-modal-target="defaultModal" data-modal-toggle="defaultModal"
+                                    data-problem-id="{{ $question->id }}">削除</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <!-- Main modal -->
+            <div id="defaultModal" tabindex="-1" aria-hidden="true"
+                class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                以下の問題を削除しますか？
+                            </h3>
+                            <button type="button"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="defaultModal">
+                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-6 space-y-6">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            項目名
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            内容
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            問題名
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            Silver
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            問題文
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $question->content }}
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            選択肢１
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            選択肢１
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            選択肢２
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            選択肢２
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            選択肢３
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            選択肢３
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            画像
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $question->image }}
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            補足
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $question->supplement }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- Modal footer -->
+                        <div
+                            class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <form action="http://localhost/admin/delete/{{ $question->id }}">
+                                <button data-modal-hide="defaultModal" type="button"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    type="submit">削除する</button>
+                            </form>
+                            <button data-modal-hide="defaultModal" type="button"
+                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">削除しない</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+        @else
+            <div class="p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+                role="alert">
+                <span class="font-medium">注意！</span> 登録されている問題はありません
+            </div>
+        @endisset
+</div>
 @endsection
