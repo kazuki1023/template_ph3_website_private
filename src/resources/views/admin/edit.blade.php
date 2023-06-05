b3650b360ba229635b06337ac3578f006d651dc1
kazuki1023 <k.keio12569@gmail.com>
2023-06-06 03:24:34 +0900

    編集画面作成

--- src/resources/views/admin/edit.blade.php
+++ src/resources/views/admin/edit.blade.php
@@ -1,3 +1,87 @@
 @extends('layouts.admin')
 @section('content')
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-4 mt-3">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        項目名
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        内容
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        問題番号
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $question->id }}
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        問題文
+                    </th>
+                    <td class="px-6 py-4">
+                      <div class="">
+                        <input type="text" id="default-input"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value="{{ $question->content }}">
+                    </div>
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        選択肢１
+                    </th>
+                    <td class="px-6 py-4">
+                        選択肢１
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        選択肢２
+                    </th>
+                    <td class="px-6 py-4">
+                        選択肢２
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        選択肢３
+                    </th>
+                    <td class="px-6 py-4">
+                        選択肢３
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        画像
+                    </th>
+                    <td class="px-6 py-4">
+                      <div class="">
+                        <input type="text" id="default-input"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value="{{ $question->id }}">
+                    </div>
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        補足
+                    </th>
+                    <td class="px-6 py-4">
+                      <div class="">
+                        <input type="text" id="default-input"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value="{{ $question->supplement }}">
+                    </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
