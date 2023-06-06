07c44b829fdfeb19d593ea08383d2002703009ba
kazuki1023 <k.keio12569@gmail.com>
2023-06-06 11:14:25 +0900

    add 正解の選択肢を選べる

--- src/resources/views/admin/edit.blade.php
+++ src/resources/views/admin/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
-    <form action="http://localhost/admin/update/{{ $question->id}}" method="POST">
-      @csrf
+    <form action="http://localhost/admin/update/{{ $question->id }}" method="POST">
+        @csrf
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-4 mt-3">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -20,7 +20,9 @@
                             問題番号
                         </th>
                         <td class="px-6 py-4">
-                          <input type="text" id="disabled-input" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ $question->id}}" disabled name="id">
+                            <input type="text" id="disabled-input" aria-label="disabled input"
+                                class=" bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value="{{ $question->id }}" disabled name="id">
                         </td>
                     </tr>
                     <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
@@ -35,11 +37,18 @@
                             </div>
                         </td>
                     </tr>
-                    @foreach ($choices as $choice)
+                    @foreach ($choices as $i => $choice)
                         <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                選択肢 {{ $choice->id }}
+                                選択肢 {{ $i + 1 }}
+                                @if ($choice->valid == 1)
+                                    <span
+                                        class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">正解</span>
+                                @else
+                                    <span
+                                        class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">不正解</span>
+                                @endif
                             </th>
                             <td class="px-6 py-4">
                                 <input type="text" id="default-input"
@@ -48,6 +57,29 @@
                             </td>
                         </tr>
                     @endforeach
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            正解の選択肢
+                        </th>
+                        <td class="px-6 py-4">
+                            <ul
+                                class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                @foreach($choices as $i => $choice)
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="horizontal-list-radio-license-{{ $i + 1}}" type="radio" value="{{ $choice->id }}"
+                                            name="list-radio"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                            @if($choice->valid == 1) checked @endif
+                                            >
+                                        <label for="horizontal-list-radio-license-{{ $i + 1}}"
+                                            class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">選択肢{{ $i + 1}}</label>
+                                    </div>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </td>
+                    </tr>
                     <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             画像
