4a42c4fcbf08ba29b4ba95f071dc9775f10e1551
kazuki1023 <k.keio12569@gmail.com>
2023-06-07 17:15:19 +0900

    fix 詳細画面のルーティング

--- src/resources/views/admin/detail.blade.php
+++ src/resources/views/admin/detail.blade.php
@@ -95,9 +95,9 @@
     <div class="content-center mt-2 flex justify-center">
         <button type="button"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onclick="location.href='http://localhost/admin/edit/{{ $question->id }}'">編集する</button>
+            onclick="location.href='{{ route('admin.edit' ,['id' => $question->id])}}'">編集する</button>
         <button type="button"
             class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-            onclick="location.href='http://localhost/admin/delete/{{ $question->id }}'">削除する</button>
+            onclick="location.href='{{ route('admin.delete', ['id' => $question->id])}}'">削除する</button>
     </div>
 @endsection
