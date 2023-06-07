5ab81cbc35c911436c626bd34e6aadde273e5011
kazuki1023 <k.keio12569@gmail.com>
2023-06-07 10:40:15 +0900

    add 編集画面で画像登録できる

--- src/resources/views/admin/edit.blade.php
+++ src/resources/views/admin/edit.blade.php
@@ -86,9 +86,9 @@
                         </th>
                         <td class="px-6 py-4">
                             <div class="">
-                                <input type="text" id="default-input"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    value="{{ $question->image }}" name="image">
+                                <img src="{{ asset('/storage/img/questions/' . $question->image)}}">
+                                <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" name="image" accept=".jpg, .svg, .png">
+                                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, or JPG .</p>
                             </div>
                         </td>
                     </tr>
