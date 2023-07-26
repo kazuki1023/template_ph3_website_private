e67c4eebfe9ca048d79b3588285e68d1baa9ff37
kazuki1023 <k.keio12569@gmail.com>
2023-07-26 13:03:56 +0900

    add pagination

--- src/app/Http/Controllers/QuestionController.php
+++ src/app/Http/Controllers/QuestionController.php
@@ -15,7 +15,7 @@ class QuestionController extends Controller
     //問題一覧を表示する
     public function show()
     {
-        $questions = Question::all();
+        $questions = Question::paginate(15);
         return view('admin.index', ['questions' => $questions]);
     }
 
