bc4003eed422d76237c816113f040c6f8c15b9aa
kazuki1023 <k.keio12569@gmail.com>
2023-07-27 12:15:15 +0900

    fix choicesテーブルと繋げて問題を表示

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Question;
+use App\Models\Choice;
 
 class QuizController extends Controller
 {
     public function show() {
-        $questions = Question::paginate(20);
-        return view('quiz', ['question' => $questions]);
+        $questions = Question::with('choices')->get();
+        return view('quiz', ['questions' => $questions]);
     }
 }
