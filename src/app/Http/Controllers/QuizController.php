c6f84ce1f216ae4826d4a8079b58e10169f36cab
kazuki1023 <k.keio12569@gmail.com>
2023-07-27 12:00:45 +0900

    add modelからデータを取得

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class QuizController extends Controller
 {
     public function show() {
-        return view('quiz');
+        $questions = Question::paginate(20);
+        return view('quiz', ['question' => $questions]);
     }
 }
