b312ccdbf18803859f7a0eca19ec2d62ca1cfe71
kazuki1023 <k.keio12569@gmail.com>
2023-08-13 14:47:03 +0900

    add 論理削除

--- src/app/Models/Question.php
+++ src/app/Models/Question.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Question extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public $timestamps = true;
     public function choices() : HasMany
